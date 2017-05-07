283352b15d1597a52c30f690ba410b75338c5913
Alex Simonian <alex.simonian@gmail.com>
2017-05-06 22:26:33 -0500

    Update

--- Chp6/HelloWorldGUI1.java
+++ Chp6/HelloWorldGUI1.java
@@ -1,11 +1,15 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class HelloWorldGUI1 {
+
+   private static String input;
    
    public static void main(String[] args) {
       JOptionPane.showMessageDialog( null, "Hello World!" );
-      JOptionPane.showMessageDialog( null, "Hello World!" );
-      JOptionPane.showMessageDialog( null, "Hello World!" );
+      JOptionPane.showConfirmDialog( null, "Do you read me?");
+      input = JOptionPane.showInputDialog( null, "Is this the real life?");
+      System.out.println(input);
    }
 
 }
+
--- Chp6/HelloWorldGUI2.java
+++ Chp6/HelloWorldGUI2.java
@@ -7,7 +7,7 @@ public class HelloWorldGUI2 {
 	private static class HelloWorldDisplay extends JPanel {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.drawString( "Hello World!", 20, 30);
+			g.drawString( "Hello World!", 50, 30);
 		}
 	}
 
@@ -24,12 +24,18 @@ public class HelloWorldGUI2 {
 		ButtonHandler listener = new ButtonHandler();
 		okButton.addActionListener(listener);
 
+		// The basic JPanel is, again, just a blank rectangle. 
+		// There are two ways to make a useful JPanel: 
+		// The first is to add other components to the panel; 
+		// the second is to draw something in the panel.
 		JPanel content = new JPanel();
 		content.setLayout(new BorderLayout());
 		content.add(displayPanel, BorderLayout.CENTER);
-		content.add(okButton, BorderLayout.SOUTH);
+		content.add(okButton, BorderLayout.EAST);
 
-		JFrame window = new JFrame("GUI TEST");
+		//  A JFrame is an independent window that can, 
+		//  for example, act as the main window of an application. 
+		JFrame window = new JFrame("This is a cool test!");
 		window.setContentPane(content);
 		window.setSize(250, 100);
 		window.setLocation(100, 100);
